f4aaa5cba491341a38cb0e67a109831847832e78
Surya Teja <suryateja@example.com>
2026-06-10 17:19:40 +0530

    added css for about button

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import HeroSection from './HeroSection'
-import About from './About'
-import Work from './Work'
-import ContactUs from './ContactUs'
-import People from './People'
-import Journey from './Journey'
+import React from "react";
+import HeroSection from "./HeroSection";
+import About from "./About";
+import Work from "./Work";
+
+import People from "./People";
+
 import "../../Styles/LandingPage.css";
-
-
 
 function LandingPage() {
   return (
-   <>
-    <HeroSection/>
-    <About/>
-    <Work/>
-    {/* <Journey/> */}
-    <People/>
-    {/* <ContactUs/>  */}
-   </>
-
-   
-  )
+    <>
+      <HeroSection />
+      <About />
+      <Work />
+      {/* <Journey/> */}
+      <People />
+      {/* <ContactUs/>  */}
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
