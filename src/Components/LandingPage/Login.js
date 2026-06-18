6e3556b3ee636af353bff3d75969b119bb023901
Surya Teja <suryateja@example.com>
2026-06-18 10:31:33 +0530

    added login css

--- src/Components/LandingPage/Login.js
+++ src/Components/LandingPage/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -6,6 +6,14 @@ import "react-toastify/dist/ReactToastify.css";
 import "../../Styles/AdminLogin.css";
 
 function Login() {
+
+   useEffect(() => {
+    document.body.classList.add("login-page");
+
+    return () => {
+      document.body.classList.remove("login-page");
+    };
+  }, []);
 
   const navigate = useNavigate();
 
